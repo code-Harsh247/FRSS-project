89ac77b4a719e059e3bd138d2570f57196aafa58
iamabhinav-dev <abhiroy7079@kgpian.iitkgp.ac.in>
2024-03-18 19:32:49 +0530

    adminloginpage done

---
+++ frontend/src/components/adminLogicSection/AdminLoginForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./AdminLoginForm.css"
+import "../assets/fonts/fonts.css"
+import hideIcon from "../assets/Icons/hide.png"
+import viewIcon from "../assets/Icons/view.png"
+import InputBox from "../InputBox/InputBox";
+import CustomButton from "../Button/CustomButton";
+
+
+
+const AdminLoginForm = () => {
+
+    const [emailInput, setEmailInput] = useState("");
+    const [passwordInput, setPasswordInput] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleEmailChange = (email) => {
+        setEmailInput(email);
+    }
+    const handlePasswordChange = (password) => {
+        setPasswordInput(password);
+    }
+
+    const handleButtonClick = (event) => {
+        event.preventDefault();  //Prevents page refresh when button is clicked. (Happens when button is inside a form element)
+        console.log("Email : ", emailInput);
+        console.log("Password : ", passwordInput);
+    };
+
+
+    return (
+        <div className="Container">
+            <div className="LoginSectionContainer">
+                <div className="FormContainer">
+                    <form>
+                        <div className="LoginHeader">
+                            <p>Admin Login</p>
+                            <span>Login to monitor and manage backend</span>
+                        </div>
+                        <div className="Email">
+                            <span> Email Address</span>
+                            <InputBox onInputChange={handleEmailChange} autocomplete="email" />
+                        </div>
+                        <div className="Password">
+                            <div><span> Password</span>
+                                <img
+                                    src={showPassword ? viewIcon : hideIcon}
+                                    alt={showPassword ? "Hide" : "Show"}
+                                    className="toggle-password-button"
+                                    onClick={togglePasswordVisibility}
+                                />
+                            </div>
+                            <InputBox onInputChange={handlePasswordChange} type={showPassword ? "text" : "password"} autocomplete="current-password" />
+                        </div>
+                        <div className="LoginBtn">
+                            <CustomButton btnText="Log in" handleClick={handleButtonClick} />
+                        </div>
+                        
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default AdminLoginForm;
--- frontend/src/pages/AdminLoginPage.js
+++ frontend/src/pages/AdminLoginPage.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import AdminLoginForm from '../components/adminLogicSection/AdminLoginForm'
+import './Css/AdminLoginPage.css';
+import AdminLoginPageImg from '../components/assets/images/adminPage_Img.jpg';
+import Logo from "../components/assets/Logo/Logo.png"
 
  const AdminLoginPage = () => {
   return (
-    <div>AdminLogin</div>
+    <div className='Container'>
+        <div className='ImgContainer'>
+          <img src= {AdminLoginPageImg} alt='Admin Login Page'/>
+        </div>
+        <AdminLoginForm/>
+        <img src={Logo} className="AdminLoginPage-Logo" alt="Company Logo" />
+    </div>
   )
 }
 export default AdminLoginPage;
+
+// import React from 'react'
+
+
+
+// const LoginPage = () => {
+//   return (
+    
+//   )
+// }
+// export default LoginPage;
