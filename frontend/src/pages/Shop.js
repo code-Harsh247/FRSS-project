adf88cff34e918e4d7dc369557f6543d1ccf2846
iamabhinav-dev <abhiroy7079@kgpian.iitkgp.ac.in>
2024-03-23 19:24:34 +0530

    shop page with navbar,shopbanner and footer and product card created

--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar';
+import BRsection from '../components/BrowseRangeSection/BRsection';
+import bannerImg from '../components/assets/images/homepage banner img.jpg'
+import CustomButton from '../components/Button/CustomButton';
+import Footer from '../components/Footer/Footer';
+import { useNavigate } from 'react-router-dom';
+import AboutUs from '../components/AboutUs/AboutUs';
+import ShopBanner from '../components/ShopBanner/ShopBanner';
+import ProductCard from '../components/ProductCard/ProductCard';
 
- const Shop = () => {
-  return (
-    <div>Shop</div>
-  )
+const Shop = () => {
+
+    const navigate = useNavigate();
+
+    const goToShop = ()=>{
+        navigate('/shop');
+    }
+
+    return (
+        <div className='container'>
+            
+                <Navbar />
+                <ShopBanner/>
+            <Footer/>
+   
+        </div>
+        
+    )
 }
+
 export default Shop;
