60eae89e969f975f7d3fa3005160d698821b8194
code-Harsh247 <justharsh2407@gmail.com>
2024-03-23 23:52:59 +0530

    Commented out Product card component in Shop page and removed red borders from homepage

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
     id: {
-        type: Number,
+        type: String,
         required: true
     },
     name: {
--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -7,7 +7,7 @@ import Footer from '../components/Footer/Footer';
 import { useNavigate } from 'react-router-dom';
 import AboutUs from '../components/AboutUs/AboutUs';
 import ShopBanner from '../components/ShopBanner/ShopBanner';
-import ProductCard from '../components/ProductCard/ProductCard';
+// import ProductCard from '../components/ProductCard/ProductCard';
 
 const Shop = () => {
 
