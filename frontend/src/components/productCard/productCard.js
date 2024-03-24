d17afbf745fe944709cf760a884e16cbf7a99acc
iamabhinav-dev <abhiroy7079@kgpian.iitkgp.ac.in>
2024-03-24 20:15:29 +0530

    Product card finalised

--- frontend/src/components/productCard/productCard.js
+++ frontend/src/components/productCard/productCard.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import './ProductCard.css';
-
-const ProductCard = ({imgURL, title, price}) => {
-    
-    return (
-        <div className="card">
-            <div className="image">
-                
-                    <img src={imgURL}/>
-                
-            </div>
-        <span className="title">
-            {title}
-        </span>
-        <span className="price">
-            {price}
-        </span>
-        </div>
-    );
+function Card() {
+  return (
+    <div className="ShopProductCard">
+      <img src="https://cdn.pixelspray.io/v2/black-bread-289bfa/Zu3Ns5/wrkr/t.resize(h:450,w:500)/data/pottery-barn/24062022img/7911181_2.jpg" alt="Pottery Barn Balboa Upholstered Swivel Armchair" />
+      <div className="card-content">
+        <h3 className="card-title">Balboa Upholstered Swivel Armchair</h3>
+        <p className="card-text">
+          <br />
+          $1000.00
+          <br />
+          Contract Grade
+        </p>
+      </div>
+    </div>
+  );
 }
-export default ProductCard;
+
+export default Card;
--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -7,7 +7,7 @@ import Footer from '../components/Footer/Footer';
 import { useNavigate } from 'react-router-dom';
 import AboutUs from '../components/AboutUs/AboutUs';
 import ShopBanner from '../components/ShopBanner/ShopBanner';
-// import ProductCard from '../components/ProductCard/ProductCard';
+import ProductCard from '../components/ProductCard/ProductCard';
 
 const Shop = () => {
 
@@ -23,6 +23,8 @@ const Shop = () => {
                 <Navbar />
                 <ShopBanner/>
             <Footer/>
+            <ProductCard/>
+            <ProductCard/>
    
         </div>
         
