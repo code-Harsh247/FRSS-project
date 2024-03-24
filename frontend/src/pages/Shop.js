f7d94a67a71cb93dc5a905079f83c4a00dae668a
iamabhinav-dev <abhiroy7079@kgpian.iitkgp.ac.in>
2024-03-24 23:24:57 +0530

    servicebanner done

--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar';
-// import BRsection from '../components/BrowseRangeSection/BRsection';
-// import bannerImg from '../components/assets/images/homepage banner img.jpg'
-// import CustomButton from '../components/Button/CustomButton';
+import BRsection from '../components/BrowseRangeSection/BRsection';
+import bannerImg from '../components/assets/images/homepage banner img.jpg'
+import CustomButton from '../components/Button/CustomButton';
 import Footer from '../components/Footer/Footer';
 import { useNavigate } from 'react-router-dom';
-// import AboutUs from '../components/AboutUs/AboutUs';
+import AboutUs from '../components/AboutUs/AboutUs';
 import ShopBanner from '../components/ShopBanner/ShopBanner';
-import ServiceBanner from '../components/ServiceBanner/ServiceBanner';
+// import ProductCard from '../components/ProductCard/ProductCard';
 
 const Shop = () => {
 
     const navigate = useNavigate();
-    const goToHome = ()=>{
-        navigate('/');
+
+    const goToShop = ()=>{
+        navigate('/shop');
     }
 
     return (
-        <div className='Shop-Container'>
+        <div className='container'>
             
-                {/* <Navbar />
+                <Navbar />
                 <ShopBanner/>
-
-            <Footer/> */}
-            <ServiceBanner/>
+            <Footer/>
    
         </div>
         
