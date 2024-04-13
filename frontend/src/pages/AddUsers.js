9339ebb73d3a56ec1d9d9550e57d6f0be891177c
iamabhinav-dev <abhiroy7079@kgpian.iitkgp.ac.in>
2024-04-13 15:39:21 +0530

    add user linked to backend

--- frontend/src/pages/AddUsers.js
+++ frontend/src/pages/AddUsers.js
@@ -4,44 +4,92 @@ import AdminNavbar from "../components/AdminNavbar/AdminNavbar";
 import AdminBanner from "../components/AdminBanner/AdminBanner";
 import { useAdminAuth } from "../context/AdminAuthContext";
 import AdminLoginPage from "./AdminLoginPage";
+import axios from "../context/axiosConfig";
 
 
 const AddUsers = () => {
-    const [userName, setUserName] = useState("");
-    const [userPhone, setUserPhone] = useState("");
-    const [userEmail, setUserEmail] = useState("");
-    const [userPassword, setUserPassword] = useState("");
+    const [nameInput, setnameInput] = useState("");
+    const [phoneNumber, setphoneNumber] = useState("");
+    const [emailInput, setemailInput] = useState("");
+    const [passwordInput, setpasswordInput] = useState("");
+    
+    
+    
 
-    const handleUserNameChange = (e) => {
-        setUserName(e.target.value);
+
+    const handlenameInputChange = (e) => {
+        setnameInput(e.target.value);
     };
 
-    const handleUserPhoneChange = (e) => {
-        setUserPhone(e.target.value);
+    const handlephoneNumberChange = (e) => {
+        setphoneNumber(e.target.value);
     };
 
-    const handleUserEmailChange = (e) => {
-        setUserEmail(e.target.value);
+    const handleemailInputChange = (e) => {
+        setemailInput(e.target.value);
     };
 
-    const handleUserPasswordChange = (e) => {
-        setUserPassword(e.target.value);
+    const handlepasswordInputChange = (e) => {
+        setpasswordInput(e.target.value);
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        console.log({
-            name: userName,
-            phone: userPhone,
-            email: userEmail,
-            password: userPassword,
-        });
-
-        setUserName("");
-        setUserPhone("");
-        setUserEmail("");
-        setUserPassword("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();  //Prevents page refresh when button is clicked. (Happens when button is inside a form element)
+        try {
+            // Check if name, phone, email, and password are entered
+            if (!nameInput || !phoneNumber || !emailInput || !passwordInput) {
+                return alert('Please fill in all the fields');
+            }
+    
+            // Validate phone number format
+            const phoneRegex = /^\d{10}$/;
+            if (!phoneRegex.test(phoneNumber)) {
+                return alert('Please enter a valid phone number (10 digits)');
+            }
+    
+            // Validate email format
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(emailInput)) {
+                return alert('Please enter a valid email address');
+            }
+    
+            // Validate password format: At least one number, one capital letter, and at least 8 characters
+            const passwordRegex = /^(?=.*\d)(?=.*[A-Z]).{8,}$/;
+            if (!passwordRegex.test(passwordInput)) {
+                return alert('Password must contain at least one number, one capital letter, and be at least 8 characters long');
+            }
+    
+            const response = await axios.post('/users/signup', {
+                name: nameInput,
+                phone: phoneNumber,
+                email: emailInput,
+                password: passwordInput,
+                cart: []
+            });
+    
+            if (response.status === 200) {
+                alert('User Added Successfully');
+                const token = response.data.token; // Access token from response data
+                localStorage.setItem('token', token);
+                console.log(response.data);
+            } else {
+                const errorMessage = response.data.errors || 'Signup failed';
+                alert(errorMessage);
+            }
+        } catch (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                alert(`Error: ${error.response.data.errors}`);
+            } else if (error.request) {
+                // The request was made but no response was received
+                alert('No response received from the server. Please try again.');
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                alert('An error occurred. Please try again.');
+            }
+        }
+        
     };
     const { isAdminLoggedIn } = useAdminAuth();
     if (isAdminLoggedIn) {
@@ -57,8 +105,8 @@ const AddUsers = () => {
                         </div>
                         <input
                             type="text"
-                            value={userName}
-                            onChange={handleUserNameChange}
+                            value={nameInput}
+                            onChange={handlenameInputChange}
                             required
                         />
                         <div className="UserPhone">
@@ -66,8 +114,8 @@ const AddUsers = () => {
                         </div>
                         <input
                             type="text"
-                            value={userPhone}
-                            onChange={handleUserPhoneChange}
+                            value={phoneNumber}
+                            onChange={handlephoneNumberChange}
                             required
                         />
                         <div className="UserEmail">
@@ -75,8 +123,8 @@ const AddUsers = () => {
                         </div>
                         <input
                             type="email"
-                            value={userEmail}
-                            onChange={handleUserEmailChange}
+                            value={emailInput}
+                            onChange={handleemailInputChange}
                             required
                         />
                         <div className="UserPassword">
@@ -84,8 +132,8 @@ const AddUsers = () => {
                         </div>
                         <input
                             type="password"
-                            value={userPassword}
-                            onChange={handleUserPasswordChange}
+                            value={passwordInput}
+                            onChange={handlepasswordInputChange}
                             required
                         />
                         <button type="submit">Add User</button>
